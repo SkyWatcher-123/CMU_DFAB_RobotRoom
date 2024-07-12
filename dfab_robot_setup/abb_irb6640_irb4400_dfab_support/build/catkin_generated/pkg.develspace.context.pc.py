104364e6e5abe7024c89b9fd42b24712a18809f1
SkyWatcher-123 <112517055+SkyWatcher-123@users.noreply.github.com>
2024-07-12 16:42:25 -0400

    Delete dfab_robot_setup/abb_irb6640_irb4400_dfab_support directory

--- dfab_robot_setup/abb_irb6640_irb4400_dfab_support/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "abb_irb6640_irb4400_dfab_support"
-PROJECT_SPACE_DIR = "/home/vina/catkin_ws/src/abb_irb6640_irb4400_dfab_support/build/devel"
-PROJECT_VERSION = "0.0.0"
--- dfab_robot_setup/abb_irb6640_irb4400_dfab_support/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/vina/catkin_ws/src/abb_irb6640_irb4400_dfab_support/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
