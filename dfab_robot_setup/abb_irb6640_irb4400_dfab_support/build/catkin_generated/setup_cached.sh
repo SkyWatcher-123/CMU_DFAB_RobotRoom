1420292b7684f62abb35712a256ed2881ec7ede9
SkyWatcher-123 <112517055+SkyWatcher-123@users.noreply.github.com>
2024-07-12 16:50:35 -0400

    Add files via upload

---
+++ dfab_robot_setup/abb_irb6640_irb4400_dfab_support/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vina/catkin_ws/src/abb_irb6640_irb4400_dfab_support/build/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/vina/.local/bin:/home/vina/miniconda3/condabin:/usr/lib/ccache:/home/vina/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/vina/catkin_ws/src/abb_irb6640_irb4400_dfab_support/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/vina/catkin_ws/src/abb_irb6640_irb4400_dfab_support/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/vina/catkin_ws/src/abb_irb6640_irb4400_dfab_support:$ROS_PACKAGE_PATH"
---
+++ dfab_robot_setup/abb_irb6640_irb4400_dfab_support/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/vina/catkin_ws/src/abb_irb6640_irb4400_dfab_support/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
